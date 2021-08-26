94525d1755c87b0685fe7eec6dae56b61d965eb2
FillipeRibeiro <fillipe@imaginationmedia.com>
2021-08-26 01:44:54 -0300

    Transform hash generate into a service class

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -2,31 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\GenerateHashService;
+
 class HashController extends Controller
 {
-    public function generateHash(string $inputText)
+    public function generateHash(string $inputText): array
     {
-        $countable = 0;
-
-        do {
-            $key = $this->generateRandomString();
-            $wordhash = $inputText . $key;
-            $hash = md5($wordhash);
-            $countable++;
-        } while (substr($hash, 0, 4) !== '0000');
-
-        return [
-            'key' => $key,
-            'hash' => $hash,
-            'try' => $countable,
-            'word_hash' => $wordhash,
-            'Input_string' => $inputText,
-        ];
-    }
-
-    private function generateRandomString(): string
-    {
-        $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        return substr(str_shuffle($letters), 0, 8);
+        return (new GenerateHashService())->handle($inputText);
     }
 }
