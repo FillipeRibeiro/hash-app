acf1f2e890a52f5097832cffe276b9423db6240a
FillipeRibeiro <fillipe@imaginationmedia.com>
2021-08-26 14:59:51 -0300

    Step three - Creating endpoint to return hashes saved

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HashInfo;
 use App\Service\GenerateHashService;
 
 class HashController extends Controller
 {
     public function generateHash(string $inputText): array
     {
-        return (new GenerateHashService())->handle($inputText);
+        $countable = 0;
+
+        do {
+            $key = $this->generateRandomString();
+            $wordConcat = $inputText . $key;
+            $hash = md5($wordConcat);
+            $countable++;
+        } while (substr($hash, 0, 4) !== '0000');
+
+        return [
+            'key' => $key,
+            'hash' => $hash,
+            'try' => $countable,
+            'word_concat' => $wordConcat,
+            'input_string' => $inputText,
+        ];
+    }
+
+    public function getHashes()
+    {
+        $filterAttempts = request()->attempts;
+
+        $hashes = HashInfo::select("id as numero_bloco, batch, string, key");
+        if (!is_null($filterAttempts)) {
+            $hashes->where('try' <= $filterAttempts);
+        }
+        return $hashes->paginate();
+    }
+
+    private function generateRandomString(): string
+    {
+        $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle($letters), 0, 8);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/hash/{inputText}', 'HashController@generateHash')
     ->name('hash.generate');
+
+Route::get('/hashes', 'HashController@getHashes');
